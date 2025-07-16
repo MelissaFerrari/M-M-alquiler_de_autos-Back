6997599792b85cc317aa79b0dce3d628d2ed70f6
MelisaDomene <domenemelisa@gmail.com>
2025-07-16 00:35:20 -0300

    Se creó un formulario para agregar autos y se agrega un auto

--- AgenciaAutos/src/main/java/AgenciaAutros/AgenciaAutos/AgenciaAutosApplication.java
+++ AgenciaAutos/src/main/java/AgenciaAutros/AgenciaAutos/AgenciaAutosApplication.java
@@ -3,6 +3,7 @@ package AgenciaAutros.AgenciaAutos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AgenciaAutosApplication {
 
